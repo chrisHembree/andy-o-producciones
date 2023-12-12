e70c4d6ba243d16dfe139475de76f472047116d2
Chris Hembree <chembree08@gmail.com>
2023-12-12 16:02:46 -0600

    new stuff!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RetratosComponent } from './retratos/retratos.component';
-import { ReelComponent } from './reel/reel.component';  // Adjust the path
+import { ReelComponent } from './reel/reel.component';
 import { ContactoComponent } from './contacto/contacto.component';
 import { HomepageComponent } from './homepage/homepage.component';
 
 
 const routes: Routes = [
-  { path: 'home', component: HomepageComponent },
+  { path: '', component: HomepageComponent },
   { path: 'retratos', component: RetratosComponent },
   { path: 'reel', component: ReelComponent },
   { path: 'contacto', component: ContactoComponent },
-  // Add more routes as needed
-  { path: '', redirectTo: '/cinematografia', pathMatch: 'full' }, // Redirect to cinematografia by default
+
+  { path: '', redirectTo: '/', pathMatch: 'full' },
 ];
 
 @NgModule({
