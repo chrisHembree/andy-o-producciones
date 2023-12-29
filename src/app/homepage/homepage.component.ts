6376d5afa74c53c1f82ab4340a7697c35b52c384
Chris Hembree <chembree08@gmail.com>
2023-12-28 18:44:27 -0600

    changed upload location and added logic in homepagecomponent

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { UploadService } from '../upload.service';
+import { Entry } from '../entry.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-homepage',
@@ -9,6 +11,25 @@ import { UploadService } from '../upload.service';
 export class HomepageComponent {
 
   constructor(private UploadService: UploadService) {}
+
+  entry: Entry = new Entry(null /* provide a File */, 'Optional Caption');
+
+  onFileChange(event: any) {
+    const fileList: FileList = event.target.files;
+
+    if (fileList.length > 0) {
+      this.entry.image = fileList[0];
+    }
+  }
+
+  submitForm() {
+    // Handle form submission here, you can send the entry to a service or perform other actions.
+    console.log('Submitted Entry:', this.entry);
+
+
+    this.entry = new Entry(null, '');
+  }
+
 
 
 }
