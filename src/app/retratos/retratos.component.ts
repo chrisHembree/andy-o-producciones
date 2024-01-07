d4b60d9b64a551e16f221941d5e26bd934a2e658
Chris Hembree <chembree08@gmail.com>
2024-01-06 21:20:56 -0600

    added image upload functionality to retratos component

--- src/app/retratos/retratos.component.ts
+++ src/app/retratos/retratos.component.ts
@@ -10,6 +10,40 @@ export class RetratosComponent {
 
   constructor(private UploadService: UploadService) {}
 
+  imageUrl: string | ArrayBuffer = '';
+
+    onFileChange(event: any) {
+      const fileList: FileList = event.target.files;
+
+      if (fileList.length > 0) {
+        const file: File = fileList[0];
 
 
-}
+        this.previewImage(file);
+
+
+        this.UploadService.uploadFile(file).subscribe(
+          (response) => {
+
+            this.imageUrl = response.imageUrl;
+            console.log('File uploaded successfully. Image URL:', this.imageUrl);
+          },
+          (error) => {
+            console.error('Error uploading file:', error);
+          }
+        );
+      }
+    }
+
+
+    private previewImage(file: File) {
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        this.imageUrl = e.target.result;
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
+
