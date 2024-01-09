946a6a68fd9646ba9a0531a13722eb28e7855343
Chris Hembree <chembree08@gmail.com>
2024-01-08 21:17:28 -0600

    upload ability added for reel component

--- src/app/reel/reel.component.ts
+++ src/app/reel/reel.component.ts
@@ -8,4 +8,45 @@ import { UploadService } from '../upload.service';
 })
 export class ReelComponent {
 
+  ReelUrl: string | ArrayBuffer = '';
+
+  constructor(private uploadService: UploadService) {}
+
+  onFileChange(event: any) {
+    const fileList: FileList = event.target.files;
+
+    if (fileList.length > 0) {
+      const file: File = fileList[0];
+
+
+      this.previewImage(file);
+
+
+      this.uploadService.uploadFile(file).subscribe(
+        (response) => {
+
+          this.ReelUrl = response.imageUrl;
+          console.log('File uploaded successfully. Image URL:', this.ReelUrl);
+        },
+        (error) => {
+          console.error('Error uploading file:', error);
+        }
+      );
+    }
+  }
+
+
+  private previewImage(file: File) {
+    const reader = new FileReader();
+
+    reader.onload = (e: any) => {
+      this.ReelUrl = e.target.result;
+    };
+
+    reader.readAsDataURL(file);
+  }
 }
+
+
+
+
