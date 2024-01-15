6e3d315c0c98adeeff2dafce08946bd082eae14a
Chris Hembree <chembree08@gmail.com>
2024-01-14 23:21:46 -0600

    multiple images now can be on screen from same function!

--- src/app/retratos/retratos.component.ts
+++ src/app/retratos/retratos.component.ts
@@ -4,21 +4,26 @@ import { UploadService } from '../upload.service';
 @Component({
   selector: 'app-retratos',
   templateUrl: './retratos.component.html',
-  styleUrl: './retratos.component.css'
+  styleUrls: ['./retratos.component.css']
 })
 export class RetratosComponent {
 
-  url="https://static.vecteezy.com/system/resources/previews/016/916/479/original/placeholder-icon-design-free-vector.jpg";
+  imageArray: string[] = [];
 
-  onSelect(event){
-    if(event.target.files[0])
-{
-   let reader = new FileReader();
-   reader.readAsDataURL(event.target.files[0]);
-   reader.onload = (event:any)=>{
-    this.url = event.target.result;
-   }
+  url = "https://static.vecteezy.com/system/resources/previews/016/916/479/original/placeholder-icon-design-free-vector.jpg";
 
+  onSelect(event) {
+    if (event.target.files && event.target.files[0]) {
+      let reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = (event: any) => {
+        this.imageArray.push(event.target.result);
+      };
+    }
   }
+
+  selectImage() {
+    // Trigger the file input
+    document.getElementById('img').click();
   }
 }
