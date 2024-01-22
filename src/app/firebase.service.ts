ea4af490f4e54eb8fecb3a0cea14417e10ef6354
Chris Hembree <chembree08@gmail.com>
2024-01-21 21:41:31 -0600

    put in fech and update methods for firebase service

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Injectable({
   providedIn: 'root'
 })
-export class FirebaseService {
 
-  constructor() { }
+export class FirebaseService {
+  constructor(private db: AngularFireDatabase) {}
+
+
+
+
+
+
+
+
+
+
+  getContactData(): any {
+    return this.db.object('/contactData').valueChanges();
+  }
+
+  updateContactData(contactData: any): Promise<void> {
+    return this.db.object('/contactData').update(contactData);
+  }
 }
