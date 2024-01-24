5696c4ad52b451cccf53bc44d5c69dd4c6b76126
Chris Hembree <chembree08@gmail.com>
2024-01-23 21:07:27 -0600

    fixed problem with firebase imports!

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -1,15 +1,12 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from '@angular/fire/database';
-import { getDatabase, ref, set } from "firebase/database";
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { getDatabase, ref, set } from 'firebase/database';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class FirebaseService {
   constructor(private db: AngularFireDatabase) {}
-
-
 
   getContactData(): any {
     return this.db.object('/contactData').valueChanges();
@@ -19,13 +16,10 @@ export class FirebaseService {
     return this.db.object('/contactData').update(contactData);
   }
 
-
   writeCaption(caption: string): Promise<void> {
-    // Use AngularFireDatabase for writing captions
-    return this.db.list('captions').push({ caption });
+    const db = getDatabase();
+    return set(ref(db, 'captions/' + caption), {
+      caption: caption
+    });
   }
-
-
-
-
 }
